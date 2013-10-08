e7b2a782af4d99e69b92e4ed40d95381535fd2b2
sideral <alejandro@slopeone.net>
2013-10-08 05:23:59 -0500

    Fixed bug with prototypes

--- assets/socketwar.js
+++ assets/socketwar.js
@@ -16,19 +16,23 @@ var SocketWar = (function(){
 		//Let's initialize our WebSocket object. This does not open the connection.
 		_webSocket = new WebSocket("ws://localhost:8080");
 		
-		_myPlayer = new SocketWar.Player('c5');
+		var playerType = 'd3';
+		_myPlayer = new SocketWar.Player(playerType + ' myself');
 		_grid = new SocketWar.Grid('#playground', 6, 14);
 		_grid.addPlayer(_myPlayer);
 		
-		SocketWar.KeyboardController.bindMove(_onMove);
+		SocketWar.KeyboardController.listenKeys(_onKeyEvent);
 		
 	}
 		
-	var _onMove = function(direction){
+	var _onKeyEvent = function(direction){
+		if(direction === 'shot'){
+			return false;
+		}
 		var offsetMap = {
 			left: {x:-1, y:0},
 			right: {x:1, y:0},
-			top: {x:0, y:-1},
+			up: {x:0, y:-1},
 			down: {x:0, y:1}
 		};
 		_grid.movePlayer(_myPlayer, offsetMap[direction]);
@@ -50,23 +54,21 @@ var SocketWar = (function(){
 SocketWar.Grid = function(playground, rows, cols){this.init(playground,rows,cols);}
 SocketWar.Grid.prototype = (function(){
 
-	var _obj, _players, _cellDimension, _rows, _cols, _playground;
-	
 	var _init = function(playground, rows, cols){
 		
-		_rows = rows;
-		_cols = cols;
-		_playground = $(playground);
-		_players = {};
+		this._rows = rows;
+		this._cols = cols;
+		this._playground = $(playground);
+		this._players = {};
 		
 		//Create the grid of the specified size.
-		_obj = $('<div>').addClass('grid');
+		this._obj = $('<div>').addClass('grid');
 		for(var i = 0; i < rows; i++){
 			for(var j = 0; j < cols; j++){
-				_obj.append($('<div>').addClass('cell'));
+				this._obj.append($('<div>').addClass('cell'));
 			}
 		}
-		_playground.html(_obj);
+		this._playground.html(this._obj);
 		
 	};
 	
@@ -77,51 +79,77 @@ SocketWar.Grid.prototype = (function(){
 	 * @returns Array
 	 */
 	var _addPlayer = function(player, pos){
+
 		//Generates random position if not found.
-		var position = pos || {x:Math.floor(Math.random() * _cols), y:Math.floor(Math.random() * _rows)}
+		var position = pos || {x:Math.floor(Math.random() * this._cols), y:Math.floor(Math.random() * this._rows)}
 		
 		//Prevent out of range positions.
-		if(position.x >= _cols || position.y >= _rows){
+		if(position.x >= this._cols || position.y >= this._rows){
 			position = {x:0, y: 0}
 		}
 		
 		//Adds player to the list, if there is no position specified, generate a random one
-		_players[player.getId()] = {
+		this._players[player.getId()] = {
 			player: player,
 			position: position
 		}
 		
-		var domCell = _obj.children().first();
+		this._playground.append(player.getDomObject());
+		return _positionatePlayer.apply(this, [player, position]);
+		
+	}
+	
+	var _removePlayer = function(player){
+		if(typeof this._players[player.getId()] === 'undefined'){
+			return;
+		}
+		//Removes dom object
+		this._playground.remove(player.getDomObject());
+		//Removes player from the list
+		delete this._players[player.getId()];
+	}
+	
+	/**
+	 * 
+	 * @param SocketWar.Player player
+	 * @param Object offset
+	 * @returns {undefined}
+	 */
+	var _movePlayer = function(player, offset){
+		
+		var currentPosition = this._players[player.getId()].position;
+		var newPosition = {
+			x: currentPosition.x + offset.x,
+			y: currentPosition.y + offset.y
+		}
+		
+		//Check boudaries
+		if(newPosition.x == this._cols || newPosition.x < 0 || newPosition.y == this._rows || newPosition.y < 0){
+			return;
+		}
+		
+		this._players[player.getId()].position = newPosition;
+		return _positionatePlayer.apply(this, [player, newPosition]);
+		
+	}
+	
+	var _positionatePlayer = function(player, position){
+		var domCell = this._obj.children().first();
 		var domPlayer = player.getDomObject();
 		var cellWidth =  domCell.width() + parseInt(domCell.css('border-left-width')) + parseInt(domCell.css('border-right-width'));
 		var cellHeight = domCell.height() + parseInt(domCell.css('border-top-width')) + parseInt(domCell.css('border-bottom-width'))
-		
-		_playground.append(domPlayer);
-			
+
 		domPlayer.css('left', cellWidth * position.x + (cellWidth - domPlayer.width())/2);
 		domPlayer.css('top', cellHeight * position.y + (cellHeight - domPlayer.height())/2);
 		
 		return position;
 	}
 	
-	var _removePlayer = function(player){_playerPosition[i*rows + j]
-		if(typeof _players[player.getId()] === 'undefined'){
-			return;
-		}
-		//Removes dom object
-		player.getDomObject();
-		//Removes player from the list
-		delete _players[player.getId()];
-	}
-	
-	var _movePlayer = function(player, offset){
-		
-	}
-	
 	return {
 		init: _init,
 		addPlayer: _addPlayer,
-		removePlayer: _removePlayer
+		removePlayer: _removePlayer,
+		movePlayer: _movePlayer
 	};
 })();
 
@@ -131,19 +159,17 @@ SocketWar.Grid.prototype = (function(){
 SocketWar.Player = function(cssClass){this.init(cssClass);};
 SocketWar.Player.prototype = (function(){
 
-	var _obj, _id;
-
 	var _init = function(cssClass){
-		_obj = $('<div>').addClass('player').addClass(cssClass);
-		_id = ++SocketWar.Player.Count;
+		this._obj = $('<div>').addClass('player').addClass(cssClass);
+		this._id = ++SocketWar.Player.Count;
 	}
 	
 	var _getId = function(){
-		return _id;
+		return this._id;
 	}
 	
 	var _getDomObject = function(){
-		return _obj;
+		return this._obj;
 	}
 	
 	return {
@@ -164,12 +190,22 @@ SocketWar.KeyboardController = (function(){
 			37: 'left',
 			38: 'up',
 			39: 'right',
-			40: 'down'
+			40: 'down',
+			65: 'left',
+			83: 'down',
+			68: 'right',
+			87: 'up',
+			74: 'left',
+			73: 'up',
+			76: 'right',
+			75: 'down',
+			77: 'shot',
+			88: 'shot'
 		}
 	/**
 	 * Let's start the game
 	 */
-	var _bindMove = function(callback){
+	var _listenKeys = function(callback){
 		$(document).keydown(_onKeyDown).keyup(_onKeyUp);
 		_callback = callback;
 	}
@@ -195,7 +231,7 @@ SocketWar.KeyboardController = (function(){
 	}
 	
 	return {
-		bindMove: _bindMove,
+		listenKeys: _listenKeys,
 	}
 })();
 
